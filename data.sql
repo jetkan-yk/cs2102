4cb7c355896ade1fcb67e0409d304e697ae5547b
Jet Kan <jetkan.yk@gmail.com>
2021-02-26 16:55:15 +0800

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Courses VALUES
+('1', 'Advanced SQL'),
+('2', 'Programming Methodology'),
+('3', 'Data Structures and Algorithms'),
+('4', 'Quantum Computing'),
+('5', 'Social Media Computing');
