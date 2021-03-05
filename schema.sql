62dc86984492064a2ecce1cf8f1f33e505c08850
Jet Kan <jetkan.yk@gmail.com>
2021-03-05 21:36:45 +0800

    Create preliminary tables

--- schema.sql
+++ schema.sql
@@ -1,7 +1,81 @@
-DROP TABLE IF EXISTS Courses CASCADE;
+DROP TABLE IF EXISTS Courses,
+Course_areas,
+Offerings,
+Sessions,
+Rooms,
+Belongs,
+Offers,
+Consists,
+Conducts;
 
-CREATE TABLE Courses(
+/* duration refers to number of hours */
+CREATE TABLE Courses (
   course_id integer,
   title text,
+  description text,
+  duration integer,
   PRIMARY KEY (course_id)
+);
+
+CREATE TABLE Course_areas (name text, PRIMARY KEY (name));
+
+CREATE TABLE Offerings (
+  launch_date date,
+  start_date date,
+  end_date date,
+  registration_deadline date,
+  fees integer,
+  seating_capacity integer,
+  target_number_registrations integer,
+  PRIMARY KEY (launch_date)
+);
+
+CREATE TABLE Sessions (
+  sid integer,
+  date date,
+  start_time time,
+  end_time time,
+  PRIMARY KEY (sid)
+);
+
+CREATE TABLE Rooms (
+  rid integer,
+  location text,
+  seating_capacity integer,
+  PRIMARY KEY (rid)
+);
+
+CREATE TABLE Belongs (
+  course_id integer,
+  name text,
+  PRIMARY KEY (course_id, name),
+  FOREIGN KEY (course_id) REFERENCES Courses,
+  FOREIGN KEY (name) REFERENCES Course_areas
+);
+
+CREATE TABLE Offers (
+  course_id integer,
+  launch_date date,
+  PRIMARY KEY (course_id, launch_date),
+  FOREIGN KEY (course_id) REFERENCES Courses,
+  FOREIGN KEY (launch_date) REFERENCES Offerings
+);
+
+CREATE TABLE Consists (
+  course_id integer,
+  launch_date date,
+  sid integer,
+  PRIMARY KEY(course_id, launch_date, sid),
+  FOREIGN KEY(course_id) REFERENCES Courses,
+  FOREIGN KEY(launch_date) REFERENCES Offerings,
+  FOREIGN KEY(sid) REFERENCES Sessions
+);
+
+CREATE TABLE Conducts (
+  sid integer,
+  rid integer,
+  /* eid integer, */
+  PRIMARY KEY(sid, rid),
+  FOREIGN KEY(sid) REFERENCES Sessions,
+  FOREIGN KEY(rid) REFERENCES Rooms
 );
