c08733c53a933a80217bce951d7f89aeb9c58087
Jet Kan <jetkan.yk@gmail.com>
2021-03-09 02:56:20 +0800

    Update schema

--- schema-jet.sql
+++ schema-jet.sql
@@ -6,7 +6,7 @@ Rooms,
 Sessions;
 
 CREATE TABLE Course_areas (
-    area_name text,
+    area_name TEXT,
     PRIMARY KEY (area_name)
 );
 
@@ -16,11 +16,11 @@ CREATE TABLE Course_areas (
  - can only be conducted from 9am to 6pm
  - cannot be conducted during 12pm to 2pm */
 CREATE TABLE Courses (
-    course_id   integer,
-    area_name   text NOT NULL,
-    title       text NOT NULL,
-    description text,
-    duration    smallint NOT NULL
+    course_id   SERIAL,
+    area_name   TEXT NOT NULL,
+    title       TEXT NOT NULL,
+    description TEXT,
+    duration    SMALLINT NOT NULL
                 CONSTRAINT duration_between_1_and_7
                 CHECK (duration BETWEEN 1 AND 7),
     PRIMARY KEY (course_id),
@@ -29,69 +29,77 @@ CREATE TABLE Courses (
         ON UPDATE CASCADE
 );
 
-/* TODO: routine update start_date and end_date */
-/* TODO: routine update seating_capacity */
-/* TODO: trigger check reg_deadline <= start_date + 10 */
-/* TODO: trigger check seating_capacity >= target_num_reg */
-/* TODO: trigger check Offerings has total participation in Sessions */
+/* TODO: trigger update start_date and end_date */
+/* TODO: trigger update seating_capacity */
+/* TODO: trigger abort Offerings if no Sessions created */
 CREATE TABLE Offerings (
-    offering_id      integer,
-    course_id        integer,
-    launch_date      date    NOT NULL,
-    start_date       date,
-    end_date         date,
-    reg_deadline     date    NOT NULL
+    offering_id      INTEGER,
+    course_id        INTEGER NOT NULL,
+    launch_date      DATE    NOT NULL,
+    start_date       DATE,
+    end_date         DATE,
+    reg_deadline     DATE    NOT NULL
                      CONSTRAINT launch_date_before_reg_deadline
                      CHECK (launch_date < reg_deadline),
-    fees             integer NOT NULL
+    fees             INTEGER NOT NULL
                      CONSTRAINT non_negative_fees
                      CHECK (target_num_reg >= 0),
-    seating_capacity integer,
-    target_num_reg   integer NOT NULL
+    seating_capacity INTEGER,
+    target_num_reg   INTEGER NOT NULL
                      CONSTRAINT non_negative_target_num_reg
                      CHECK (target_num_reg >= 0),
     PRIMARY KEY (offering_id),
     FOREIGN KEY (course_id) REFERENCES Courses
         ON DELETE CASCADE,
-    UNIQUE (course_id, launch_date)
+    CONSTRAINT offerings_cand_key
+    UNIQUE (course_id, launch_date),
+    CONSTRAINT valid_reg_deadline
+    CHECK (reg_deadline <= start_date + 10),
+    CONSTRAINT sufficient_seating_capacity
+    CHECK (seating_capacity >= target_num_reg)
 );
 
 CREATE TABLE Instructors (
-    eid integer,
+    eid INTEGER,
     PRIMARY KEY (eid)
 );
 
 CREATE TABLE Rooms (
-    rid              integer,
-    location         text,
-    seating_capacity integer NOT NULL
+    rid              INTEGER,
+    location         TEXT,
+    seating_capacity INTEGER NOT NULL
                      CONSTRAINT non_negative_seating_capacity
                      CHECK (seating_capacity >= 0),
     PRIMARY KEY (rid)
 );
 
-/* TODO: auto assign session_id for an offering starts from 1 */
-/* TODO: check end_time = start_time + duration */
+/* TODO: trigger check room availability */
+/* TODO: trigger system generate session_id for an offering starts from 1, see:
+    https://stackoverflow.com/questions/29996639/autoincrement-attribute-based-on-another-attribute */
+/* TODO: trigger auto assign eid */
+/* TODO: trigger auto assign end_time = start_time + duration + lunch break */
 /* Sessions can take lunch break, e.g. 4 hour session from 10am to 4pm */
 /* date & time in ISO 8601 format */
 /* eid is the instructor id */
 CREATE TABLE Sessions (
-    offering_id  integer,
-    session_id   integer,
-    session_date date NOT NULL,
-    start_time   time
+    offering_id  INTEGER,
+    session_id   SERIAL, --INTEGER
+    session_date DATE NOT NULL,
+    start_time   TIME NOT NULL
                  CONSTRAINT valid_start_time
                  CHECK (start_time BETWEEN '09:00' AND '11:00'
                      OR start_time BETWEEN '14:00' AND '17:00'),
-    end_time     time
+    end_time     TIME --NOT NULL
                  CONSTRAINT valid_end_time
                  CHECK (end_time BETWEEN '10:00' AND '12:00'
                      OR end_time BETWEEN '15:00' AND '18:00'),
-    eid          integer,
-    rid          integer,
+    eid          INTEGER, --NOT NULL
+    rid          INTEGER NOT NULL,
     PRIMARY KEY (offering_id, session_id),
     FOREIGN KEY (offering_id) REFERENCES Offerings
         ON DELETE CASCADE,
     FOREIGN KEY (eid) REFERENCES Instructors,
-    FOREIGN KEY (rid) REFERENCES Rooms
+    FOREIGN KEY (rid) REFERENCES Rooms,
+    CONSTRAINT sessions_cand_key
+    UNIQUE (offering_id, session_date, start_time)
 );
