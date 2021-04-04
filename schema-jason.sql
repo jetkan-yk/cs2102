4f55e04402170d10ebfed90a0d55ab02fca439b2
Jason <jasontay9645@gmail.com>
2021-04-05 02:45:33 +0800

    Update schema-jason.sql

--- schema-jason.sql
+++ schema-jason.sql
@@ -1,22 +1,24 @@
 DROP TABLE IF EXISTS Pay_slips,
 Employees,
-EmployeePayslips,
 Instructors,
 Part_time_Employees,
-Part_time_instructors,
+Part_time_Instructors,
 Full_time_Employees,
-Full_time_instructors,
+Full_time_Instructors,
 Managers,
 Administrators,
 Manages,
 Specializes;
 
+/*An Employee has to be either a part_time_Employee or full_time_Employee*/
+/*ISA Instructor An Employee COULD (may or may not) be an instructor*/
+/*ISA Part-time Employee/Full-time Employees - An Employee HAS to be either a Full-time Employee or Part-time Employee, but not both*/
 CREATE TABLE Employees (
     eid             INTEGER,
-    ename           char(20),
+    ename           TEXT,
     phone           INTEGER,
-    eaddress        char(50),
-    email           char(50),
+    eaddress        TEXT,
+    email           TEXT,
     join_date       DATE,
     depart_date     DATE,
     PRIMARY KEY (eid)
@@ -31,52 +33,59 @@ CREATE TABLE Pay_slips (
     payment_date    DATE,
     PRIMARY KEY (eid, payment_date),
     FOREIGN KEY (eid) REFERENCES Employees
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
-/*Instructors ISA Employees - not all instructors have to be employees - employees can be instructors and also other sub classes*/
-CREATE TABLE Instructors (
-    eid             INTEGER PRIMARY KEY REFERENCES Employees ON DELETE CASCADE
-);
-
-/*ISA Employee*/
+/*Employee ISA Part-time Employee/Full-time Employees - An Employee HAS to be either a Full-time Employee or Part-time Employee, but not both*/
+/*ISA Part-time Instructor - A Part-time Employee HAS to be a Part-time Instructor*/
 CREATE TABLE Part_time_Employees (
     eid             INTEGER PRIMARY KEY REFERENCES Employees ON DELETE CASCADE,
     hourly_rate     INTEGER
 );
 
-CREATE TABLE Part_time_instructors (
-    eid     INTEGER PRIMARY KEY REFERENCES Part_time_Employees ON DELETE CASCADE
-);
-
-/*ISA Employee*/
+/*ISA Full-time Instructor/Manager/Administrator - A Full-time Employee HAS to be either a Full-time Instructor, an Administrator or a Manager*/
 CREATE TABLE Full_time_Employees (
     eid             INTEGER PRIMARY KEY REFERENCES Employees ON DELETE CASCADE,
     monthly_salary  INTEGER
 );
 
-/*ISA Employee*/
-CREATE TABLE Full_time_instructors (
-    eid INTEGER PRIMARY KEY REFERENCES Full_time_Employees ON DELETE CASCADE
+/*Employees ISA Instructor - An Employee COULD (may or may not) be an instructor*/
+/*ISA Part-time Instructor/Full-time Instructor - An Instructor HAS to be either a Full-time Instructor or Part-time Instructor, but not both*/
+CREATE TABLE Instructors (
+    eid     INTEGER PRIMARY KEY REFERENCES Employees ON DELETE CASCADE
 );
 
-/*ISA Full_time_Employee*/
+/*Instructor ISA Part-time Instructor/Full-time Instructor - An Instructor HAS to be either a Full-time Instructor or Part-time Instructor, but not both*/
+/*
+CREATE TABLE Part_time_Instructors (
+    eid     INTEGER PRIMARY KEY REFERENCES Part_time_Employees ON DELETE CASCADE
+);
+*/
+
+/*Instructor, Full_time_Employee - How to enforce ISA relationship on both?*/
+CREATE TABLE Full_time_Instructors (
+    eid     INTEGER PRIMARY KEY REFERENCES Full_time_Employees ON DELETE CASCADE
+);
+
+/*Full_time_Employee*/
 CREATE TABLE Managers (
-    eid INTEGER PRIMARY KEY REFERENCES Full_time_Employees ON DELETE CASCADE
+    eid     INTEGER PRIMARY KEY REFERENCES Full_time_Employees ON DELETE CASCADE
 );
 
-/*ISA Full_time_Employee*/
+/*Full_time_Employee*/
 CREATE TABLE Administrators (
     eid     INTEGER PRIMARY KEY REFERENCES Full_time_Employees ON DELETE CASCADE
 );
 
 CREATE TABLE Manages (
     eid             INTEGER,  /*instructor id*/
-    cname           char(20),
+    cname           TEXT,
     PRIMARY KEY (cname, eid)
 );
 
 CREATE TABLE Specializes (
     eid             INTEGER,  /*instructor id*/
-    area_name       char(20),
-    PRIMARY KEY (cname, eid)
+    area_name       TEXT,
+    PRIMARY KEY (area_name, eid)
 );
