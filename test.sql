3643ac3d788695ec78b053d32a29b46c31b85219
Jet Kan <jetkan.yk@gmail.com>
2021-04-05 13:55:16 +0800

    Example test for session rid

--- test.sql
+++ test.sql
@@ -1,6 +1,6 @@
 SELECT * FROM add_course('course1', 'desc1', 'Computer Security', 4);
 
-SELECT * FROM add_session(8, 4225, '2021-07-31', '9:00');
+SELECT * FROM add_session(8, 4225, '2021-07-31', '9:00', 1);
 
 SELECT * FROM find_rooms('2021-07-31', '9:00', 7);
 
@@ -15,3 +15,12 @@ SELECT
 FROM Offerings O LEFT JOIN Sessions S
 	   ON O.course_id = S.course_id 
 		    AND O.offering_id = S.offering_id;
+
+/* To check each Room cannot be used by two Sessions at the same time */
+SELECT session_date, start_time, end_time, rid
+  FROM Sessions NATURAL JOIN Rooms
+ ORDER BY session_date, start_time, end_time;
+
+SELECT * FROM add_session(1, 1242, '2021-07-31', '09:00', 1); /* should work */
+SELECT * FROM add_session(1, 1242, '2021-07-31', '10:00', 1); /* should fail */
+SELECT * FROM add_session(1, 1242, '2021-07-01', '11:00', 3); /* should fail */
