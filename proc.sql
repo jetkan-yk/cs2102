9caf57e042c7ea687e179731ee25c2d3854000ad
Jet Kan <jetkan.yk@gmail.com>
2021-04-10 10:43:22 +0800

    Revert "Add DROP command for register_session"
    This reverts commit ab7f371247cdf49e648775e600f272cd0dc54be4.

--- proc.sql
+++ proc.sql
@@ -1415,7 +1415,6 @@ LANGUAGE SQL;
 /* 17. register_session
     This routine is used when a customer requests to register for a session in a course offering.
     RETURNS: a TEXT status  */
-DROP FUNCTION register_session(integer,integer,integer,integer,text);
 CREATE OR REPLACE FUNCTION register_session(
     _cust_id INTEGER,
     _course_id INTEGER,
